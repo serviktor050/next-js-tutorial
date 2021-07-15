33167f28d599eb6bd16653467a5577bca44a8ced
serviktor050 <mr.s-gagulin@yandex.ru>
2021-07-15 15:25:06 +0300

    Создание API мок сервера

--- next-js-tutorial/pages/posts.js
+++ next-js-tutorial/pages/posts.js
@@ -1,9 +1,22 @@
 import { MainLayout } from "../components/MainLayout";
+import { useState, useEffect } from "react";
 
 export default function Posts() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    async function load() {
+      const response = await fetch("http://localhost:4200/posts");
+      const json = await response.json();
+      setPosts(json);
+    }
+    load();
+  }, []);
+
   return (
     <MainLayout title={"Posts Page"}>
       <h1>Posts Page</h1>
+      <pre>{JSON.stringify(posts, null, 2)}</pre>
     </MainLayout>
   );
 }
