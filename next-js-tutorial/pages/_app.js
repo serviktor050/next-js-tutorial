8274d8cfa34b72fe0f87d0fafe21e31d4b7c5216
serviktor050 <mr.s-gagulin@yandex.ru>
2021-07-16 11:35:17 +0300

    Рассмотрено создание индикатора загрузки

--- next-js-tutorial/pages/_app.js
+++ next-js-tutorial/pages/_app.js
@@ -1,8 +1,16 @@
+import NextNProgress from "nextjs-progressbar";
 import "../styles/main.scss";
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
+      <NextNProgress
+        color="#29D"
+        startPosition={0.3}
+        stopDelayMs={200}
+        height={3}
+        showOnShallow={true}
+      />
       <Component {...pageProps} />
       {/* <style jsx global>
         {`
--- next-js-tutorial/pages/about/index.js
+++ next-js-tutorial/pages/about/index.js
@@ -3,16 +3,25 @@ import Router from "next/router";
 
 import { MainLayout } from "../../components/MainLayout";
 
-export default function About() {
+export default function About({ title }) {
   const linkClickHandler = () => {
     Router.push("/");
   };
 
   return (
     <MainLayout title={"About Page"}>
-      <h1>About Page</h1>
+      <h1>{title}</h1>
       <button onClick={linkClickHandler}>Go back to home</button>
       <button onClick={() => Router.push("/posts")}>Go to posts</button>
     </MainLayout>
   );
 }
+
+About.getInitialProps = async () => {
+  const responce = await fetch("http://localhost:4200/about/");
+  const data = await responce.json();
+
+  return {
+    title: data.title,
+  };
+};
